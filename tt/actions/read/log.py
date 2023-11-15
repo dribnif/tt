20ebbca4aaca722a9d23f4cac525485eaa25c14c
Chigozie Nri <chigozie@gmail.com>
2023-11-15 13:40:16 +0000

    Fix period check to allow no period

--- tt/actions/read/log.py
+++ tt/actions/read/log.py
@@ -6,21 +6,19 @@ from tt.dateutils.dateutils import *
 from tt.colors.colors import *
 
 
-def action_log(period=None):
+def action_log(period=[]):
     data = get_data_store().load()
     work = data['work']
     log = defaultdict(lambda: {'delta': timedelta()})
     current = None
-    if period is not None:
-        period = [datetime.fromisoformat(dt) for dt in period]
+    period = [datetime.fromisoformat(dt) for dt in period]
     for item in work:
         start_time = parse_isotime(item['start'])
 
-        if period is not None:
-            if start_time < period[0]:
-                continue
-            if len(period) > 1 and start_time > period[1]:
-                continue
+        if len(period) > 0 and start_time < period[0]:
+            continue
+        if len(period) > 1 and start_time > period[1]:
+            continue
 
         if 'end' in item:
             log[item['name']]['delta'] += (
